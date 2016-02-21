8a79161ded57c46eb772055bc802fe8d84950c8b
Gaurav Bhola <gaurav414u@gmail.com>
2016-02-21 22:09:29 +0530

    Removing RecyclerEndlessAdapter

--- sample/src/main/java/com/android/test/scrollviewtest/RecyclerViewFragment.java
+++ sample/src/main/java/com/android/test/scrollviewtest/RecyclerViewFragment.java
@@ -2,6 +2,7 @@ package com.android.test.scrollviewtest;
 
 import com.gauravbhola.viewpagerslidingheader.ViewPagerSlidingHeaderFragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,9 +11,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.List;
 
 /**
  * Created by gauravbhola on 18/12/15.
@@ -45,9 +48,13 @@ public class RecyclerViewFragment extends ViewPagerSlidingHeaderFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_recyclerview, null);
-        mListView = (RecyclerView) v.findViewById(R.id.recyclerview);
-        //mListView.setScrollViewCallbacks(this);
+        return v;
+    }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mListView = (RecyclerView) view.findViewById(R.id.recyclerview);
         final ArrayList<String> list = new ArrayList<>();
         int i = 100;
         do {
@@ -57,38 +64,64 @@ public class RecyclerViewFragment extends ViewPagerSlidingHeaderFragment {
 
         mLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         mListView.setLayoutManager(mLayoutManager);
-        final RecyclerEndlessAdapter mAdapter = new RecyclerEndlessAdapter(list, mListView);
-
-        mAdapter.setOnLoadMoreListener(new RecyclerEndlessAdapter.OnLoadMoreListener() {
-            @Override
-            public void onLoadMore() {
-                //add null , so the adapter will check view_type and show progress bar at bottom
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        //prepare dummy data
-                        int start = list.size();
-                        int end = start + 100;
-
-                        ArrayList<String> newData = new ArrayList<String>();
-                        for (int i = start + 1; i <= end; i++) {
-                            newData.add(i + "");
-                        }
-                        Random r = new Random();
-                        boolean showError = r.nextBoolean();
-                        if (showError) {
-                            mAdapter.setLoaded(null, "some error");
-                        } else {
-                            mAdapter.setLoaded(newData, null);
-                        }
-                        //or you can add all at once but do not forget to call mAdapter.notifyDataSetChanged();
-                    }
-                }, 2000);
-
-            }
-        });
-
-        mListView.setAdapter(mAdapter);
-        return v;
+        mListView.setAdapter(new MyAdapter(getActivity().getApplicationContext(), list));
     }
+
+    public static class MyAdapter extends RecyclerView.Adapter {
+        private Context mContext;
+        private List<String> mItems;
+
+        public MyAdapter(Context context, List<String> items) {
+            mContext = context;
+            mItems = items;
+        }
+
+        public static class MyViewHolder extends RecyclerView.ViewHolder {
+            private final TextView textView;
+
+            public static MyViewHolder newInstance(View itemView) {
+                TextView textView = (TextView) itemView.findViewById(android.R.id.text1);
+                return new MyViewHolder(itemView, textView);
+            }
+
+            private MyViewHolder(View itemView, final TextView textView) {
+                super(itemView);
+                this.textView = textView;
+                itemView.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(v.getContext(),
+                                "OnClick :" + textView.getText(),
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+
+            public void setText(CharSequence text) {
+                textView.setText(text);
+            }
+        }
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(
+                    android.R.layout.simple_list_item_1, parent, false);
+            MyViewHolder vh = MyViewHolder.newInstance(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            String text = mItems.get(position);
+            ((MyViewHolder) holder).setText(text);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+    }
+
 }
